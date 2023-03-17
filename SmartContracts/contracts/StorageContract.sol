33e0bb9e16b1939d6bdc48bf2cd87572c1553d4c
Gustavove <gustavove510@gmail.com>
2023-03-17 19:15:45 +0100

    Explorer in progress - Added to contract functions to explorer

--- SmartContracts/contracts/StorageContract.sol
+++ SmartContracts/contracts/StorageContract.sol
@@ -2,14 +2,13 @@
 
 pragma solidity ^0.8.9;
 
-contract StorageContract{ 
-
+contract StorageContract {
     address payable owner;
 
-    struct SensorData{
+    struct SensorData {
         string cid; //Identificador
-        string url; 
-        uint256 timestamp; 
+        string url;
+        uint256 timestamp;
         uint8 v;
         bytes32 r;
         bytes32 s;
@@ -31,50 +30,37 @@ contract StorageContract{
         bool created;
     }
 
-
-
     mapping(address => DataProvider) public Devices;
     address[] public DataProviderList;
 
+    event EventProviderAdded(address indexed addressProvider, uint256 date);
 
-
-    event EventProviderAdded (
-        address indexed addressProvider,
-        uint date
-    );
-
-    event EventSensorAdded (
+    event EventSensorAdded(
         address indexed addressProvider,
         string mac,
-        uint date
+        uint256 date
     );
-    
-    event EventDataAdded (
+
+    event EventDataAdded(
         string indexed cid,
         address indexed _provider_address,
         string mac,
         string url
     );
 
-
-
     constructor() {
         owner = payable(msg.sender);
     }
 
-    function newProvider (
-        address _provider_address
-    )
-    public {
-        require(payable(msg.sender) == owner, "Only contract's owner can add a provider");
-        require(Devices[_provider_address].created == false, "Provider has already added");
+    function newProvider(address _provider_address) public {
+        require( payable(msg.sender) == owner, "Only contract's owner can add a provider");
+        require( Devices[_provider_address].created == false, "Provider has already added");
 
         Devices[_provider_address].provider_address = _provider_address;
         Devices[_provider_address].created = true;
         DataProviderList.push(_provider_address);
-        
-        emit EventProviderAdded(_provider_address, block.timestamp);
 
+        emit EventProviderAdded(_provider_address, block.timestamp);
     }
 
     /*
@@ -82,24 +68,21 @@ contract StorageContract{
      * @param v, r, s and hash make up signature using the EIP-191 standard.
         The signature is generated using encode_defunct and eth.account.sign_message in python
      */
-    function newSensor (
+    function newSensor(
         string memory _mac,
         uint8 _v,
         bytes32 _r,
         bytes32 _s,
         bytes32 _hash
-    )
-    public {
+    ) public {
         address recoveredAddress = ecrecover(_hash, _v, _r, _s);
         require(Devices[recoveredAddress].created == true, "Invalid signature, provider doesn't exists");
         require(Devices[recoveredAddress].SensorInfo[_mac].created == false, "Sensor has already added");
 
         Devices[recoveredAddress].SensorInfo[_mac].mac = _mac;
-
         Devices[recoveredAddress].SensorInfoList.push(_mac);
-        
-        emit EventSensorAdded(recoveredAddress, _mac, block.timestamp);
 
+        emit EventSensorAdded(recoveredAddress, _mac, block.timestamp);
     }
 
     /*
@@ -107,7 +90,7 @@ contract StorageContract{
      * @param v, r, s and hash make up signature using the EIP-191 standard.
         The signature is generated using encode_defunct and eth.account.sign_message in python
      */
-    function newData (
+    function newData(
         string memory _mac,
         string memory _cid,
         string memory _url,
@@ -116,8 +99,7 @@ contract StorageContract{
         bytes32 _r,
         bytes32 _s,
         bytes32 _hash
-    )
-    public {
+    ) public {
         address recoveredAddress = ecrecover(_hash, _v, _r, _s);
         require(Devices[recoveredAddress].created == true, "Invalid signature, provider doesn't exists");
         require(recoveredAddress == msg.sender, "Only provider can upload new data");
@@ -125,7 +107,7 @@ contract StorageContract{
 
         Devices[recoveredAddress].SensorInfo[_mac].Data[_cid].cid = _cid;
         Devices[recoveredAddress].SensorInfo[_mac].Data[_cid].url = _url;
-        Devices[recoveredAddress].SensorInfo[_mac].Data[_cid].timestamp= _timestamp;
+        Devices[recoveredAddress].SensorInfo[_mac].Data[_cid].timestamp = _timestamp;
         Devices[recoveredAddress].SensorInfo[_mac].Data[_cid].v = _v;
         Devices[recoveredAddress].SensorInfo[_mac].Data[_cid].r = _r;
         Devices[recoveredAddress].SensorInfo[_mac].Data[_cid].s = _s;
@@ -136,4 +118,29 @@ contract StorageContract{
         emit EventDataAdded(_cid, recoveredAddress, _mac, _url);
     }
 
+    function getAllProviders() public view returns (address[] memory) {
+        return DataProviderList;
+    }
+
+    function getMacList(address _provider_address) public view returns (string[] memory) {
+        require(Devices[_provider_address].created == true, "Provider doesn't exists");
+        return Devices[_provider_address].SensorInfoList;
+    }
+
+    function getCidList(address _provider_address, string memory _mac) public view returns (string[] memory) {
+        require(Devices[_provider_address].created == true, "Invalid provider address");
+        require(Devices[_provider_address].SensorInfo[_mac].created == true, "Invalid sensor MAC");
+        
+        return Devices[_provider_address].SensorInfo[_mac].SensorDataList;
+    }
+
+    function getLastCID(address _provider_address, string memory _mac) public view returns (string memory) {
+        require(Devices[_provider_address].created == true, "Invalid provider address");
+        require(Devices[_provider_address].SensorInfo[_mac].created == true, "Invalid sensor MAC");
+
+        uint256 len = Devices[_provider_address].SensorInfo[_mac].SensorDataList.length;
+        require(len > 0, "No data available for this sensor");
+
+        return Devices[_provider_address].SensorInfo[_mac].SensorDataList[len - 1];
+    }
 }
