cf838c55cb2f41357387daa9b446351df2afa2db
Gustavove <gustavove510@gmail.com>
2023-03-29 01:24:24 +0200

    FIXED: Sign + send message to backend

--- Devices/Lora/loratrustprototype-main/LoraDevices/src/mainNode.cpp
+++ Devices/Lora/loratrustprototype-main/LoraDevices/src/mainNode.cpp
@@ -2,7 +2,6 @@
 #include "config.h"
 
 #ifdef NODE
-#include "Transaction.h"
 #include "HttpEth.h"
 #include "Wallet.h"
 #include "Utils.h"
@@ -19,32 +18,6 @@ OLED display;
 void setup() {
     Serial.begin(115200);
     Serial.println("Execution started");
-
-    //TEST FIRMA
-
-    uint8_t privateKey[32] = {0xee,0x8c,0x2c,0xf0, 0x8d, 0x19, 0x37, 0x4b, 0xeb, 0x3a, 0xa9, 0xcd, 0xa9, 0xfa, 0x53, 0xaf, 0xe2, 0x68,0xdd, 0x91, 0xb5, 0xb1, 0x5a, 0xe5, 0xf5, 0x64, 0x5a, 0x20, 0x26, 0xf1, 0x26, 0x7d};
-    uint8_t publicKey[64];
-    const struct uECC_Curve_t* curve = uECC_secp256k1();
-    uECC_compute_public_key(privateKey, publicKey, curve);
-    char message[] = "Hola";
-
-    mbedtls_sha256_context sha_ctx;
-    unsigned char message_hash[32];
-    mbedtls_sha256_init(&sha_ctx);
-    mbedtls_sha256_update(&sha_ctx, (const unsigned char *)message, strlen(message));
-    mbedtls_sha256_finish(&sha_ctx, message_hash);
-    mbedtls_sha256_free(&sha_ctx);
-
-    uint8_t signature[64];
-    //Firma no determinista, se utiliza un valor aleatorio para obtener la firma
-    uECC_sign(privateKey, message_hash, sizeof(message_hash), signature, curve);
-
-
-    if (uECC_verify(publicKey, message_hash, sizeof(message_hash), signature, curve)) {
-        Serial.println("La firma es válida");
-    } else {
-         Serial.println("La firma es inválida");
-    }
 
     //WIFI
 
@@ -118,7 +91,6 @@ void loop() {
     } else {
          Serial.println("La firma es inválida");
     }
-
 
     if (runEvery(SENDINGTIME)) { // repeat every 1000 millis
         //sign_message(0xee8c2cf08d19374beb3aa9cda9fa53afe268dd91b5b15ae5f5645a2026f1267d,"hola", 4, firma)
